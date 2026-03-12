8e1d036af6edf744f89fbbf6e4acc09b147c1622
RickyVokeh <rickyooke2@gmail.com>
2026-03-12 11:21:41 +0300

    app name rectification

--- includes/config/constants.php
+++ includes/config/constants.php
@@ -1,6 +1,6 @@
 <?php
 // Application constants
-define('APP_NAME', $_ENV['APP_NAME'] ?? 'Restaurant Booking System');
+define('APP_NAME', $_ENV['APP_NAME'] ?? 'Restaurant Booking');
 define('APP_URL', $_ENV['APP_URL'] ?? 'http://localhost');
 define('APP_ENV', $_ENV['APP_ENV'] ?? 'development');
 define('APP_DEBUG', filter_var($_ENV['APP_DEBUG'] ?? true, FILTER_VALIDATE_BOOLEAN));
